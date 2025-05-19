ef3762023f0bb47c382ed449d7035dab1929b969
Jasson Cardoza <jassonchiquillo@hotmail.com>
2025-05-19 12:02:31 -0600

    agregando funciones

--- Gestor-de-resetas/src/views/recetas/recetas.jsx
+++ Gestor-de-resetas/src/views/recetas/recetas.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import {useForm} from 'react-hook-form';
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import {db} from '../../Services/firebase/config'
+import { doc, updateDoc, deleteDoc } from "firebase/firestore"; // importo dod , updateDoc y deleteDoc de la documentacion
 
 
 export const Recetas = () => {
     const [recetas, setRecetas] = useState([]);
-    const {register, handleSubmit, watch} = useForm (); // importamos el metodo watch y reset de react hook form
-    
+    // importamos el metodo watch y reset de react hook form
+    const {register, handleSubmit, watch, reset} = useForm (); // reset sirve para limpiar mi form una vez enviado los datos
+    const [editarReceta, setEditarReceta] = useState(null);
     const getRecetas = async () => {
         
 //mostramos la informacion en consola ---- esto viene de la documentacion
@@ -42,6 +44,7 @@ const docRef = await addDoc(collection(db, "recetas"), {
 console.log("Document written with ID: ", docRef.id);
 
 getRecetas();
+reset(); 
  }
 
 
@@ -56,11 +59,41 @@ getRecetas();
 
  // editar mis recetas haciendo uso de la documentacion de firestore
 
+const updateReceta =async (idReceta, recetaActualizada) =>{  // Creo una nueva constante que abarque todo el codigo de firestore
+
+    const recetaRef = doc(db, "recetas", idReceta);
+    await updateDoc(recetaRef, recetaActualizada);
+    
+};
+
+ 
+  const onSubmit = async (data) => {
+    if (editarReceta) {
+      // modo edición
+      await updateReceta(editarReceta, data);
+      setEditarReceta(null);
+    } else {
+      // modo creación
+      await addRecetas(data);
+    }
+    reset();
+    getRecetas();
+  };
+
+
+  // funcion borrar recetas
+
+  const deleteReceta = async (idReceta) => {
+  const recetaRef = doc(db, "recetas", idReceta);
+  await deleteDoc(recetaRef);
+  getRecetas(); // Recargar lista luego de borrar
+};
+
 
     return (
         <div className="container" style={{marginTop: "10rem"}}>
-            <h2 className="mb-4">Add Recipe</h2>
-            <form onSubmit={handleSubmit(addRecetas)} className="mb-4">
+            <h2 className="mb-4">{editarReceta ? "Edit Recipe" : "Add Recipe"}</h2> {/* un ternario para al h2 */}
+            <form onSubmit={handleSubmit(onSubmit)} className="mb-4"> {/* remplazo el addRecetas por un onSubmit */}
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">
                         Recipe name
@@ -143,10 +176,10 @@ getRecetas();
                     />
                 </div>
 
-
-                <button type="submit" className="btn btn-primary">
-                    Add Recipe
+                <button type="submit" className="btn btn-primary"> 
+                    {editarReceta ? "Guardar cambios" : "Add Recipe"} {/* un ternario para el nombre del boton */}
                 </button>
+
             </form>
 
             <h3>Recipe List</h3>
@@ -168,7 +201,23 @@ getRecetas();
                             <td style={{wordWrap:"break-word" , maxWidth:"10rem"}}>{receta.steps}</td>
                             <td style={{wordWrap:"break-word" , maxWidth:"14rem", whiteSpace:"normal"}}>{receta.imgUrl}</td>
                             <td style={{wordWrap:"break-word" , maxWidth:"7rem"}}>{receta.autor}</td>
-                            <td style={{wordWrap:"break-word" , maxWidth:"3rem"}}><button className="btn btn-sm btn-warning"> Editar</button></td>
+                            <td style={{wordWrap:"break-word" , maxWidth:"3rem"}}>
+                                <button className="btn btn-sm btn-warning" onClick={() =>{
+                                setEditarReceta(receta.id);
+                                reset({
+                                name: receta.name,
+                                ingredients: receta.ingredients,
+                                steps: receta.steps,
+                                imgUrl: receta.imgUrl,
+                                autor: receta.autor
+                                });
+                                }}
+                                > Editar</button>
+                            </td>
+
+                            <td style={{wordWrap:"break-word" , maxWidth:"3rem"}}>
+                            <button className="btn btn-sm btn-danger" onClick={() => deleteReceta(receta.id)}> Borrar </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
